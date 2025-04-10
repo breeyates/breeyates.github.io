10b3e26e345f3a6bbb41c73a6ed8715db44aa819
Bree Yates <breeyates1@gmail.com>
2025-04-10 09:23:59 -0600

    fix resume download

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -4,42 +4,50 @@ import { navigationBarSx, tabsSx } from "./styles";
 import { Button } from "@mui/material";
 
 type NavigationBarProps = {
-    sectionRefs: {
-      [key: string]: React.RefObject<HTMLElement>;
-    };
+  sectionRefs: {
+    [key: string]: React.RefObject<HTMLElement>;
   };
+};
 
 const NavigationBar: React.FC<NavigationBarProps> = ({ sectionRefs }) => {
-    const [selectedTab, setSelectedTab] = useState<string | undefined>('About Me');
-    const tabs = ['Work', 'Projects', 'Contact'];
+  const [selectedTab, setSelectedTab] = useState<string | undefined>(
+    "About Me"
+  );
+  const tabs = ["Work", "Projects", "Contact"];
 
-    const handleTabClick = (tab: string) => {
-      setSelectedTab(tab);
-      const ref = sectionRefs[tab];
-      console.log(ref);
-        if (ref?.current) {
-            ref.current.scrollIntoView({ behavior: "smooth" });
-        }
-    };
+  const handleTabClick = (tab: string) => {
+    setSelectedTab(tab);
+    const ref = sectionRefs[tab];
+    console.log(ref);
+    if (ref?.current) {
+      ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
-    return (
+  return (
     <div style={navigationBarSx}>
-        <Tab text="BY" />
-        <div className="tabs" style={tabsSx}>
-          {tabs.map((tab) => (
-              <Tab
-                key={tab}
-                text={tab}
-                selected={tab === selectedTab}
-                onClick={() => handleTabClick(tab)}
-              />
-          ))}
-          <Button sx={{ padding: 0, textTransform: 'none' }} variant="text" href="/breanna-yates-resume.pdf" download component="a">
-            <Tab text="↓ Resume" sx={{textDecoration: 'underline'}} />
-          </Button>
-        </div>
+      <Tab text="BY" />
+      <div className="tabs" style={tabsSx}>
+        {tabs.map((tab) => (
+          <Tab
+            key={tab}
+            text={tab}
+            selected={tab === selectedTab}
+            onClick={() => handleTabClick(tab)}
+          />
+        ))}
+        <Button
+          sx={{ padding: 0, textTransform: "none" }}
+          variant="text"
+          href={`${process.env.PUBLIC_URL}/breanna-yates-resume.pdf`}
+          download
+          component="a"
+        >
+          <Tab text="↓ Resume" sx={{ textDecoration: "underline" }} />
+        </Button>
+      </div>
     </div>
-    );
+  );
 };
 
 export default NavigationBar;
